65c17e5fbaf6b074d20b1e1d0e7b9db28d1a6204
livinbackend <bekhanbackend@livin.kz>
2024-05-11 23:31:59 +0500

    landing

--- src/components/cerfModal/CerfModal.js
+++ src/components/cerfModal/CerfModal.js
@@ -158,7 +158,7 @@ function  CerfModal({ onClose, prices, restaurant_id }) {
                             'principal' : selectedPrice,
                         },
                         'additional_information': {
-                            'hook_url': 'http://api.reddel.kz/handle',
+                            'hook_url': 'http://api.reddel.kz:8000/handle',
                             'success_url': 'https://reddel.kz/profile',
                             'failure_url': 'https://reddel.kz/profile'
                         },
